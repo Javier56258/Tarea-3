0db597e60b06dada693fc5e37c6717ff7140cc3d
Javier Santander <javier.santander2101@alumnos.ubiobio.cl>
2024-10-05 22:57:30 -0300

    ADD validacion rut personas

--- src/validations/user.validation.js
+++ src/validations/user.validation.js
@@ -27,7 +27,8 @@ export const userBodyValidation = Joi.object({
     rut: Joi.string()
         .min(9)
         .max(12)
-        .pattern(/^\d{1,2}(\.\d{3}){2}-[\dkK]$|^\d{7,8}-[\dkK]$/)
+        .pattern(/^(2[0-9]|1[0-9]|[1-9])(\.\d{3}){2}-[\dkK]$|^\d{7,8}-[\dkK]$/)
+        //(/^(?:[1-2]?\d{1,2}(?:\.\d{3}){2}|[1-9]\d{0,2}(?:\.\d{3}){1,2})-[\dkK]$/)
         .required()
         .messages({
             "string.empty": "El rut no puede estar vacío.",
