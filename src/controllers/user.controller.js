b9756adad9b45351dbc257600be82f28efa0d6f9
Javier Santander <javier.santander2101@alumnos.ubiobio.cl>
2024-10-05 22:51:04 -0300

    Se agregaron funciones de actualizar y borrar, además se modifico el resto para que funcionara con el service

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,7 +2,7 @@
 import User from '../entity/user.entity.js';
 import {AppDataSource} from '../config/configDb.js';
 import { userBodyValidation } from '../validations/user.validation.js';
-import { createUserService, getUserService } from '../services/user.service.js';
+import { createUserService, getUserService, getUsersService, updateUserService, deleteUserService } from '../services/user.service.js';
 
 export async function createUser(req, res) {
     try {
@@ -51,22 +51,76 @@ export async function getUser(req, res) {
 
 export async function getUsers(req, res) {
     try {
-        const userRepository = AppDataSource.getRepository(User);
-
-        const users = await userRepository.find();
+        const users = await getUsersService();
 
         if(!users || users.length === 0) {
             return res.status(404).json({
                 message: "No se encontraron usuarios",
                 data: null
-            })
+            });
         }
 
         res.status(200).json({
             message: "Usuarios encontrados",
             data: users
-        })
+        });
     } catch (error) {
-        console.error('Error al obtener un usuarios, el error: ', error);
+        console.error('Error al obtener los usuarios, el error: ', error);
+    }
+}
+
+export async function updateUser(req, res) {
+    try {
+        const id = req.params.id;
+        const user = req.body;
+
+        const {value, error} = userBodyValidation.validate(user);
+
+        if (error) {
+            return res.status(400).json({
+                message: error.message
+            });
+        }
+
+        const userData = await updateUserService(id, value);
+
+        if (!userData) {
+            return res.status(404).json({
+                message: "Usuario no encontrado",
+                data: null
+            });
+        }
+
+        res.status(200).json({
+            message: "Usuario actualizado correctamente",
+            data: userData
+        });
+
+    } catch (error) {
+        console.error("Error al actualizar un usuario: ", error);
+        res.status(500).json({ message: "Error interno en el servidor" });
+    }
+}
+
+export async function deleteUser(req, res) {
+    try {
+        const id = req.params.id;
+
+        const userDeleted = await deleteUserService(id);
+
+        if (!userDeleted) {
+            return res.status(404).json({
+                message: "Usuario no encontrado",
+                data: null
+            });
+        }
+
+        res.status(200).json({
+            message: "Usuario eliminado correctamente",
+            data: userDeleted
+        });
+    } catch (error) {
+        console.error("Error al eliminar un usuario: ", error);
+        res.status(500).json({ message: "Error interno en el servidor" });
     }
 }
